64f0ee1dc3c0a6d7a4742d3d346384fd0c47e3df
TW-IT-Robby.hong <robby.hong@shoalter.com>
2022-04-08 11:59:32 +0800

    register fix

--- models/register_model.js
+++ models/register_model.js
@@ -23,24 +23,20 @@ module.exports = function register(memberData) {
           reject(result);
         } else {
           // 將資料寫入資料庫
-          db.query(
-            "INSERT INTO member_info SET ?",
-            memberData,
-            function (err, rows) {
-              // 若資料庫部分出現問題，則回傳給client端「伺服器錯誤，請稍後再試！」的結果。
-              if (err) {
-                console.log(err);
-                result.status = "註冊失敗。";
-                result.err = "伺服器錯誤，請稍後在試！";
-                reject(result);
-                return;
-              }
-              // 若寫入資料庫成功，則回傳給clinet端下：
-              result.status = "註冊成功。";
-              result.registerMember = memberData;
-              resolve(result);
+          db("INSERT INTO member_info SET ?", memberData, function (err, rows) {
+            // 若資料庫部分出現問題，則回傳給client端「伺服器錯誤，請稍後再試！」的結果。
+            if (err) {
+              console.log(err);
+              result.status = "註冊失敗。";
+              result.err = "伺服器錯誤，請稍後在試！";
+              reject(result);
+              return;
             }
-          );
+            // 若寫入資料庫成功，則回傳給clinet端下：
+            result.status = "註冊成功。";
+            result.registerMember = memberData;
+            resolve(result);
+          });
         }
       }
     );
